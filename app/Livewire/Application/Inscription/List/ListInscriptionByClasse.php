99ff00352b54881f06382793516352ddf328089d
mkbcentral <mkbcentralcode@gmail.com>
2023-12-18 21:32:50 +0100

    Fix for 18.12.2023 morning

--- app/Livewire/Application/Inscription/List/ListInscriptionByClasse.php
+++ app/Livewire/Application/Inscription/List/ListInscriptionByClasse.php
@@ -3,22 +3,75 @@
 namespace App\Livewire\Application\Inscription\List;
 
 use App\Livewire\Helpers\Inscription\GetAllInscriptionHelper;
+use App\Livewire\Helpers\SchoolHelper;
 use App\Models\Classe;
+use App\Models\Inscription;
+use App\Models\Student;
 use Livewire\Component;
 
 class ListInscriptionByClasse extends Component
 {
+    protected $listeners = [
+        'scolaryYearFresh' => 'getScolaryYear',
+        'CurrancyFresh' => 'getCurrency',
+        'refreshListInscription' => '$refresh',
+        'selectedClasseOption' => 'getOptionSelected',
+        'deleteInscriptionListner' => 'delete'
+    ];
+
     public $keyToSearch = '';
     public $classeId;
     public $inscriptions;
     public $classeData;
-    public function mount($classe){
-       $this->classeId=$classe;
-       $this->classeData=Classe::find($classe);
+    public $idInscription;
+    public $selectedIndex = 0;
+
+    public function edit(Inscription $inscription)
+    {
+        $this->selectedIndex = $inscription->classe->classeOption->id;
+        $this->dispatch('studentAndInscription', $inscription->student);
+    }
+    public function editInscription(Inscription $inscription)
+    {
+        $this->dispatch(
+            'inscriptionToEdit',
+            $inscription,
+            $this->selectedIndex == 0 ?
+                $inscription->classe->classeOption->id :
+                $this->selectedIndex
+        );
+    }
+
+    public function showDeleteDialog(string $idInscription)
+    {
+        $this->idInscription = $idInscription;
+        $this->dispatch('delete-inscription-dialog');
+    }
+    public function delete()
+    {
+        $inscription = Inscription::find($this->idInscription);
+        $student = Student::find($inscription->student_id);
+        $scolayYear = (new SchoolHelper())->getCurrentScolaryYear();
+        if ($inscription->payments->isEmpty()) {
+            $inscription->delete();
+            if ($student->scolaryYear->id == $scolayYear->id) {
+                $student->delete();
+            }
+            $this->dispatch('inscription-deleted', ['message' => "Inscription bien rétirée !"]);
+        } else {
+            $this->dispatch('inscription-deleted', ['message' => "Impossible, Cette inscription a déjà des données utilisée dans le système"]);
+        }
+    }
+
+
+    public function mount($classe)
+    {
+        $this->classeId = $classe;
+        $this->classeData = Classe::find($classe);
     }
     public function render()
     {
-        $this->inscriptions=GetAllInscriptionHelper::getListInscriptionByClasseForCurrentYear($this->classeId,$this->keyToSearch);
+        $this->inscriptions = GetAllInscriptionHelper::getListInscriptionByClasseForCurrentYear($this->classeId, $this->keyToSearch);
         return view('livewire.application.inscription.list.list-inscription-by-classe');
     }
 }
--- resources/views/livewire/application/inscription/list/list-inscription-by-classe.blade.php
+++ resources/views/livewire/application/inscription/list/list-inscription-by-classe.blade.php
@@ -1,4 +1,6 @@
 <div>
+    @livewire('application.inscription.forms.edit-inscription-form')
+    @livewire('application.inscription.forms.edit-classe-and-inscription')
     <div>
         <div class="content-header">
             <div class="container-fluid">
@@ -17,46 +19,91 @@
             </div>
         </div>
         @if ($inscriptions->isEmpty())
-            <span class="text-success text-center p-4">
-                <h4><i class="fa fa-database" aria-hidden="true"></i>
-                    Aucune donnée trouvée !
-                </h4>
-            </span>
+        <span class="text-success text-center p-4">
+            <h4><i class="fa fa-database" aria-hidden="true"></i>
+                Aucune donnée trouvée !
+            </h4>
+        </span>
         @else
-            <div class="card p-2 w-50">
-                <h4><span class="">Classe:
-                    </span>{{ $classeData->name . '/' . $classeData->classeOption->name }}</h4>
-            </div>
-            <div class="d-flex justify-content-between align-items-center">
-                <x-form.search-input  wire:model.live.debounce.500ms="keyToSearch" />
-                <a target="_blank" href="{{ route('print.list.inscription.by.classe', $classeData->id) }}"><i
-                        class="fa fa-print" aria-hidden="true"></i> Imprimer la liste</a>
-            </div>
-            <table class="table table-stripped table-sm mt-2">
-                <thead class="thead-light">
-                    <tr class="text-uppercase">
-                        <th class="text-center">#</th>
-                        <th>Noms élève</th>
-                        <th class="text-center">Genre</th>
-                        <th class="text-center">Age</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($inscriptions as $index => $inscription)
-                        <tr>
-                            <td class="text-center">{{ $index + 1 }}</td>
-                            <td>{{ $inscription->student->name . '/' . $inscription->classe->name . ' ' . $inscription->classe->classeOption->name }}
-                            </td>
+        <div class="card p-2 w-50">
+            <h4><span class="">Classe:
+                </span>{{ $classeData->name . '/' . $classeData->classeOption->name }}</h4>
+        </div>
+        <div class="d-flex justify-content-between align-items-center">
+            <x-form.search-input wire:model.live.debounce.500ms="keyToSearch" />
+            <a target="_blank" href="{{ route('print.list.inscription.by.classe', $classeData->id) }}"><i
+                    class="fa fa-print" aria-hidden="true"></i> Imprimer la liste</a>
+        </div>
+        <table class="table table-stripped table-sm mt-2">
+            <thead class="thead-light">
+                <tr class="text-uppercase">
+                    <th class="text-center">#</th>
+                    <th>Noms élève</th>
+                    <th class="text-center">Genre</th>
+                    <th class="text-center">Age</th>
+                    <th class="text-center">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($inscriptions as $index => $inscription)
+                <tr>
+                    <td class="text-center">{{ $index + 1 }}</td>
+                    <td>{{ $inscription->student->name . '/' . $inscription->classe->name . ' ' .
+                        $inscription->classe->classeOption->name }}
+                    </td>
 
-                            <td class="text-center">{{ $inscription->student->gender }}
-                            </td>
-                            <td class="text-center">
-                                {{ $inscription->student->getAge($inscription->student->date_of_birth) }}
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    <td class="text-center">{{ $inscription->student->gender }}
+                    </td>
+                    <td class="text-center">
+                        {{ $inscription->student->getAge($inscription->student->date_of_birth) }}
+                    </td>
+                    <td class="text-center">
+                        @can('valid-payment')
+                        <x-form.button wire:click='editInscription({{ $inscription }})' class="btn-sm text-secondary"
+                            type="button" data-toggle="modal" data-target="#editClasseAnInscription">
+                            <i class="fa fa-cog" aria-hidden="true"></i>
+                        </x-form.button>
+                        <x-form.button wire:click='edit({{ $inscription }})' class="btn-sm" type="button"
+                            data-toggle="modal" data-target="#formEditInscriptionModal">
+                            <i class="fas fa-edit text-primary"></i>
+                        </x-form.button>
+                        <x-form.button class=" btn-sm" type="button"
+                            wire:click="showDeleteDialog({{ $inscription->id }})">
+                            <i class="fa fa-trash text-danger" aria-hidden="true"></i>
+                        </x-form.button>
+                        @endcan
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
         @endif
     </div>
+    @push('js')
+    <script type="module">
+        //Confirmation dialog for delete role
+        window.addEventListener('delete-inscription-dialog', event => {
+                                        Swal.fire({
+                                            title: 'Voulez-vous vraimant ',
+                                            text: "supprimer ?",
+                                            icon: 'warning',
+                                            showCancelButton: true,
+                                            confirmButtonColor: '#3085d6',
+                                            cancelButtonColor: '#d33',
+                                            confirmButtonText: 'Yes'
+                                        }).then((result) => {
+                                            if (result.isConfirmed) {
+                                                Livewire.dispatch('deleteInscriptionListner');
+                                            }
+                                        })
+                                    })
+                                    window.addEventListener('inscription-deleted', event => {
+                                        Swal.fire(
+                                            'Oprétion !',
+                                            event.detail[0].message,
+                                            'success'
+                                        );
+                                    });
+    </script>
+    @endpush
 </div>
