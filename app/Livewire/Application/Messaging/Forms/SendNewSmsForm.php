35eaff1d9e7a6ad635772626ce07afe0ca0689f3
mkbcentral <mkbcentralcode@gmail.com>
2024-02-14 09:20:44 +0100

    Make content for sender sms

--- app/Livewire/Application/Messaging/Forms/SendNewSmsForm.php
+++ app/Livewire/Application/Messaging/Forms/SendNewSmsForm.php
@@ -25,12 +25,17 @@ class SendNewSmsForm extends Component
             SmsNotificationHelper::sendSMS(
                 '+243898337969',
                 '+243' . $this->studentResponsable->phone,
-                'C.S.'.auth()->user()->school->name."\n".$this->body."\nA: ".date('Y-m-d H:i:s')
+                'C.S.' . auth()->user()->school->name . "\n" . $this->body . "\nA: " . date('Y-m-d H:i:s')
             );
             $this->dispatch('added', ['message' => 'Message bien envoyer']);
         } catch (Exception $ex) {
             $this->dispatch('error', ['message' => $ex->getMessage()]);
         }
+    }
+
+    public function mount()
+    {
+        $this->body = "La direction du C.S AQUILA vous informe qu’à partir de ce Mercredi 14/02/2024, il y aura une détente de fin du 1er Trimestre de deux jours, la reprise est prévue le Lundi 19/02/2024à 7h30’ .";
     }
     public function render()
     {
